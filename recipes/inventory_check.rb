df69ccc716d980c9f382293de9078bb6acb0c4e7
Matt Ray <matthewhray@gmail.com>
2015-09-25 11:59:53 -0600

    added the looping logic for running the inventory checks

--- recipes/inventory_check.rb
+++ recipes/inventory_check.rb
@@ -16,21 +16,22 @@ if lastcheck.nil? || (now - interval) > lastcheck
   log 'Running the inventory check' do
     level :info
   end
+
+  # out of policy, iterate over mounts to run inventory checks
+  # hpsum --report --location (requires baseline)
+  # node[hpsum][bl].keys.each do |baseline|
+  #   baseline.keys.each do |pkg|
+  #     hpsum_package pkg do
+  #       action pkg[action]
+  #       version pkg[version]
+  #       allow_retry true
+  #       allow_reboot true
+  #     end
+  # end
+
   node.override['hpsum']['inventory']['lastcheck'] = now
 else
   log 'No need to run the inventory check, still in policy.' do
     level :info
   end
 end
-
-# if out of policy, iterate over mounts to run inventory checks
-# hpsum --report --location (requires baseline)
-# node[hpsum][bl].keys.each do |baseline|
-#   baseline.keys.each do |pkg|
-#     hpsum_package pkg do
-#       action pkg[action]
-#       version pkg[version]
-#       allow_retry true
-#       allow_reboot true
-#     end
-# end
